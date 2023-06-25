b7fb6a6abde2e7b08ada6c802f1512e971288e22
Antony F Montesinos <afmm101294@gmail.com>
2023-06-25 19:13:51 -0300

    Context

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,25 @@
 import ItemCount from "../ItemCount/ItemCount";
 import styles from './ItemDetail.module.css';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom'
+
+import { CartContext } from "../../context/CartContext";
 
 
 const ItemDetail = ({id, name, price, category, img, stock, description}) => {
     const [quantityAdded, setQuantityAdded] = useState(0)
 
+    const { addItem } = useContext(CartContext)
+
     const handleOnAdd = (quantity) => {
         setQuantityAdded(quantity)
-     }
+
+        const item = {
+            id, name, price
+        }
+
+        addItem(item, quantity)
+    }
 
     return(
         <article className={styles.CardItem}>
